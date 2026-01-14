00c700f21775f36d825ac3dada1b3217e0e6cc10
Bruno <alvalade@live.fr>
2026-01-14 07:37:58 +0100

    Nettoyage et rangement du projet en sections dédiées.FIN Du Projet.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+//*********************************************************
+// Données*/
+//****************************************************** */
 const slides = [
 	{
 		"image": "slide1.jpg",
@@ -16,53 +19,29 @@ const slides = [
 		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-
- let indexSlide = 0
-
 //*********************************************************
-// Ecouteurs sur les fleches de gauche et droite */
+// État*/
 //****************************************************** */
-
+let indexSlide = 0;
+//*********************************************************
+// DOM (élements de la page) */
+//****************************************************** */
 let btnFlecheGauche = document.querySelector(".arrow_left")
-
-btnFlecheGauche.addEventListener("click", () => {
-	console.log("t'as bien cliqué sur la flèche gauche");
-	indexSlide--;
-	if (indexSlide < 0) {indexSlide = slides.length - 1}
-	console.log(indexSlide);
-	slider(slides, indexSlide);
-
-});
-
 let btnFlecheDroite = document.querySelector(".arrow_right")
-
-btnFlecheDroite.addEventListener("click", () => {
-	console.log("T'as bien cliqué sur la flèche de droite");
-	indexSlide++;
-	if (indexSlide >= slides.length) {indexSlide = 0;}
-	console.log(indexSlide);
-	slider(slides, indexSlide);
-
-});
-
-
-
-let sectionDots = document.querySelector(".dots");//dit que la variable JS sectionDots est la classe css html .dots
-
-
+let sectionDots = document.querySelector(".dots");
+let imageSlides = document.querySelector(".banner-img");
+let descriptionSlide = document.querySelector("#banner p");
+//*********************************************************
+// Fonctions*/
+//****************************************************** */
 function genererDots(slides) {
 	for (let i = 0; i < slides.length; i++) { //on initialise la BOUCLE donne l'arret et on l'incrémente
-
-		//********************************************************************************************
-		//on créé toutes les variables necessaires pour créer le html
-		//********************************************************************************************
-
 
 		let boutonDot = document.createElement("button");// crée une balise button
 		boutonDot.classList.add("dot");// ajoute la class dot au boutonDot
 
 		if (i === 0) {                  // si [i] est égal a 0 
-			boutonDot.classList.add("dot_selected")// alors on la class devient devient dot_selected
+			boutonDot.classList.add("dot_selected")// alors la class devient devient dot_selected
 		};
 
 		sectionDots.appendChild(boutonDot);//ajoute boutonDot a la class html .dots ici sectionDots
@@ -71,30 +50,45 @@ function genererDots(slides) {
 
 }
 
-genererDots(slides)
+function GenererSlider(slides, indexSlide) {
 
-
-let imageSlides = document.querySelector(".banner-img");
-let descriptionSlide = document.querySelector("#banner p");
-
-function slider(slides, indexSlide) {
-
+    imageSlides.src = `./assets/images/slideshow/${slides[indexSlide].image}`; // donne le chemin pour afficher la bonne image
 	
-    imageSlides.src = `./assets/images/slideshow/${slides[indexSlide].image}`; // le nomme par rapport au JSON image: images/xxxx.png
+	descriptionSlide.innerHTML = slides[indexSlide].tagLine; 
 
-	
-	descriptionSlide.innerHTML = slides[indexSlide].tagLine;
-
-	
 	const dotActuel = document.querySelector(".dot_selected");
 	dotActuel.classList.remove("dot_selected");
 
 	const listeDots = document.querySelectorAll(".dot");
 	listeDots[indexSlide].classList.add("dot_selected");
 
-
 }
+//*********************************************************
+// Événements*/
+//****************************************************** */
+btnFlecheGauche.addEventListener("click", () => {
+	console.log("t'as bien cliqué sur la flèche gauche");
+	indexSlide--;
+	if (indexSlide < 0) {
+		indexSlide = slides.length - 1};
+	console.log(indexSlide);
+	GenererSlider(slides, indexSlide);
 
-slider(slides, indexSlide);
+});
+
+btnFlecheDroite.addEventListener("click", () => {
+	console.log("T'as bien cliqué sur la flèche de droite");
+	indexSlide++;
+	if (indexSlide >= slides.length) {
+		indexSlide = 0;};
+	console.log(indexSlide);
+	GenererSlider(slides, indexSlide);
+
+});
+//*********************************************************
+// Initialisation*/
+//****************************************************** */
+genererDots(slides)
+GenererSlider(slides, indexSlide);
 
 
